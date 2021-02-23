3d563f16677edddaad239f32b9ee20377b4c89c6
Villem Alango <villem.alango@gmail.com>
2021-02-23 16:41:55 +0200

    closed #2: timing

--- src/Game.js
+++ src/Game.js
@@ -7,13 +7,26 @@ export default class Game extends React.Component {
     super(props)
     this.state = {
       movesCount: 0,
+      times: [0, 0],
       winner: null
     }
+    //  Todo: check if this is kosher to add instance properties this way.
+    this.tick = setTimeout(() => this.timerTick(), 100)
+  }
+
+  timerTick () {
+    const times = [...this.state.times]
+    times[this.state.movesCount & 1] += 0.1
+    this.setState({ times })
+    this.tick = setTimeout(() => this.timerTick(), 100)
   }
 
   commitMove (winner) {
     this.setState({ movesCount: this.state.movesCount + 1 })
-    if (winner) this.setState({ winner })
+    if (winner) {
+      clearTimeout(this.tick)
+      this.setState({ winner })
+    }
   }
 
   getPlayer () {
@@ -21,20 +34,21 @@ export default class Game extends React.Component {
   }
 
   render () {
-    const { winner } = this.state
-    const status = winner ? 'Winner: ' + winner : 'Next player: ' + this.getPlayer()
+    const { movesCount, times, winner } = this.state
+    const time = times[movesCount & 1].toPrecision(2) + 'secs'
+    const status = winner ? 'Winner: ' + winner : 'Player: ' + this.getPlayer()
 
     return (
       <div className="game">
         <div className="game-board">
           <Board
-            commitMove = {(winner) => this.commitMove(winner)}
-            getPlayer = {() => this.getPlayer()}
-            winner = {this.state.winner}
+            commitMove={(winner) => this.commitMove(winner)}
+            getPlayer={() => this.getPlayer()}
+            winner={this.state.winner}
           />
         </div>
         <div className="game-info">
-          <div className="status">{status}</div>
+          <div className="status">{status + ' (' + time + ')'}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
